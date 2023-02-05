3a4aff4a08c199be275f8c259a25abe6b751beee
meyraasahin <humeyrasahinx@gmail.com>
2023-02-06 01:28:34 +0300

    BT-12: update and delete operations were implemented on cart related services.

--- src/main/java/com/egeuniversity/Tez/Service/Cart/CartService.java
+++ src/main/java/com/egeuniversity/Tez/Service/Cart/CartService.java
@@ -6,4 +6,8 @@ import com.egeuniversity.Tez.Model.Cart.Cart.CartRequestDTO;
 public interface CartService {
     Cart getCart(Integer id);
     Cart createCart(CartRequestDTO cartRequestDTO);
+    void deleteCart(Cart cart);
+    Cart deleteCartLineItem(Integer id);
+    Cart decreaseQuantity(Integer cartLineItemId);
+    Cart increaseQuantity(Integer cartLineItemId);
 }
