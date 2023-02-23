fec0a4f5b3472b6828a9ca9ef732a10458c41c5c
meyraasahin <humeyrasahinx@gmail.com>
2023-02-23 13:46:25 +0300

    BT-30: frontend init

--- src/main/java/com/egeuniversity/Tez/Controller/UniversityController.java
+++ src/main/java/com/egeuniversity/Tez/Controller/UniversityController.java
@@ -6,12 +6,18 @@ import com.egeuniversity.Tez.Service.University.UniversityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
 
+@Named
+@ViewScoped
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/university")
-public class UniversityController {
+public class UniversityController implements Serializable {
     private final UniversityService universityService;
 
     @PostMapping(path = "/add")
