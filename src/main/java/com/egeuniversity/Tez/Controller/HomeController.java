e355fdffafc4bfab29a086fe7b678c7ac1cccc8d
Berfin-ux <berfin2019@gmail.com>
2023-02-28 18:58:06 +0300

    homepage taslak1

---
+++ src/main/java/com/egeuniversity/Tez/Controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.egeuniversity.Tez.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
